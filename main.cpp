1eb94d94b4885280d7a8d070461b9521dbc5e352
danielzelfo <daniel.zelfo@gmail.com>
2019-04-20 13:13:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-/*  Program: Automated Highway Patrol V1.0
+/*  Program: Automated Highway Patrol V1.1
 Author: Daniel Zelfo
 Class: CSCI 110
 Date: 10/22/2018
 Description:
 Calculates and Displays the appropriate ticket price for each vehicle given the current speed and the speed limit in the current zone.
 Displays calculated statistics for all analyzed vehicles at the end of the program.
+v1.1: modified on 10/23/2018 to work with two times at two checkpoints. Also formats time in 12 hour format with am/pm
 
 I certify that the code below is my own work.
 */
@@ -17,45 +18,50 @@ using namespace std;
 
 //prototypes
 void inputD(string output, double& input);
+void inputI(string output, int& input);
 void inputS(string output, string& input);
 double getFee(double overLimit);
 double calcTicketPrice(double speed, double speedLimit, int base);
-void outputTicket(int type, string plateNumber, double ticketPrice=0);
+void outputTicket(int type, string plateNumber, string timeFormated, double ticketPrice = 0);
 void outputStats(int numTicketed, int numVehicles, double totalTicketPrices);
+string getTime(int time);
 int main()
 {
-	string plateNumber;
+	string plateNumber, timeFormated;
 	double speed, speedLimit, ticketPrice, totalTicketPrices = 0;
 	const int base = 150;
-	int numVehicles = 0, numTicketed = 0;
-	cout << "Automated Highway Patrol Program v1.0\nAuthor: Daniel Zelfo\n\n";
+	const double distance = 5.0;
+	int timeA, timeB, numVehicles = 0, numTicketed = 0;
+	cout << "Automated Highway Patrol Program V1.1 (EXTRA CREDIT)\nAuthor: Daniel Zelfo\n\n";
 	inputS("Enter a license plate number  --> ", plateNumber);
 	while (plateNumber != "QUIT")
 	{
-		inputD("Enter current vehicle's speed --> ", speed);
+		inputI("Enter time at checkpoint A    --> ", timeA);
+		inputI("Enter time at checkpoint B    --> ", timeB);
 		inputD("Enter speed limit in the zone --> ", speedLimit);
 		numVehicles += 1;
+		speed = distance * 60 / (timeB - timeA);
+		timeFormated = getTime(timeB);
 		if (speed - 50 > speedLimit)
 		{
 			numTicketed += 1;
 			totalTicketPrices += 1000;
-			outputTicket(1, plateNumber);
+			outputTicket(1, plateNumber, timeFormated);
 		}
 		else if (speed - 5 >= speedLimit)
 		{
 			numTicketed += 1;
 			ticketPrice = calcTicketPrice(speed, speedLimit, base);
 			totalTicketPrices += ticketPrice;
-			outputTicket(2, plateNumber, ticketPrice);
+			outputTicket(2, plateNumber, timeFormated, ticketPrice);
 		}
 		else
 		{
-			outputTicket(3, plateNumber);
+			outputTicket(3, plateNumber, timeFormated);
 		}
 		inputS("Enter a license plate number  --> ", plateNumber);
 	}
 	outputStats(numTicketed, numVehicles, totalTicketPrices);
-
 	return 0;
 }
 
@@ -64,10 +70,32 @@ void inputD(string output, double& input)
 	cout << output;
 	cin >> input;
 }
+void inputI(string output, int& input)
+{
+	cout << output;
+	cin >> input;
+}
 void inputS(string output, string& input)
 {
 	cout << output;
 	cin >> input;
+}
+
+string getTime(int timeMin)
+{
+	string m = "am";
+	if (timeMin > 720)
+	{
+		timeMin -= 720;
+		m = "pm";
+	}
+	string hours = to_string(timeMin / 60);
+	string minutes = to_string(timeMin % 60);
+	if (hours == "0")
+		hours = "12";
+	if (minutes.length() == 1)
+		minutes = "0" + minutes;
+	return hours + ":" + minutes + m;
 }
 
 double getFee(double overLimit)
@@ -80,19 +108,20 @@ double getFee(double overLimit)
 
 double calcTicketPrice(double speed, double speedLimit, int base)
 {
-	double overLimit = speed - speedLimit;
+	double overLimit = int(speed - speedLimit);
 	double fee = getFee(overLimit);
 	return base + fee * overLimit;
 }
 
-void outputTicket(int type, string plateNumber, double ticketPrice)
+void outputTicket(int type, string plateNumber, string timeFormated, double ticketPrice)
 {
 	if (type == 1)
-		cout << "A ticket of $1000 is issued to " << plateNumber << ".\n\n";
+		cout << "A ticket of $1000";
 	else if (type == 2)
-		cout << "A ticket of $" << ticketPrice << " is issued to " << plateNumber << ".\n\n";
+		cout << "A ticket of $" << ticketPrice;
 	else
-		cout << "No ticket is issued to " << plateNumber << ".\n\n";
+		cout << "No ticket";
+	cout << " is issued to " << plateNumber << " at " << timeFormated << ".\n\n";
 }
 
 void outputStats(int numTicketed, int numVehicles, double totalTicketPrices)
@@ -101,5 +130,5 @@ void outputStats(int numTicketed, int numVehicles, double totalTicketPrices)
 	cout << "\nTickets were given to " << numTicketed << " out of " << numVehicles << " vehicles.\n";
 	cout << "Percent of ticketed vehicles: " << double(numTicketed) * 100 / numVehicles << "%\n";
 	cout << "Average ticket amount:       " << totalTicketPrices / numTicketed << endl;
-	cout << "Thank you for using our AHP program.\n";
+	cout << "Thank you for using our advance AHP program.\n";
 }
